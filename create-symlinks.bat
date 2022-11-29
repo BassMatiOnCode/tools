d9b7779768ac24e92c5d65559b5debad8e1808da
bassmationcode <BassmatiOnCode@freenet.de>
2022-11-29 14:47:12 +0100

    initial commit

---
+++ create-symlinks.bat
@@ -0,0 +1,6 @@
+cd docs
+mklink /d inc ..\..\inc\docs
+mklink logo.svg ..\..\site-root\docs\logo.svg
+mklink favicon.ico ..\..\site-root\docs\favicon.ico
+mklink site.css ..\..\site-root\docs\site.css
+mklink colors.css ..\..\site-root\docs\colors.css
---
+++ start-dev.bat
@@ -0,0 +1,3 @@
+call start-vs.bat
+call start-webserver.bat
+call start-msedge.bat
